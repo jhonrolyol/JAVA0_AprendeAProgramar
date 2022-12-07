c11063e334e380426e82aafb6be598e24145fa24
JR <rolyordonezleon@gmail.com>
2022-12-07 08:45:25 -0500

    Poo
    Mi primer programa en Java

---
+++ 01_PooJava/src/pkg01_poojava/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+// Link del curso: https://www.youtube.com/watch?v=ajyy8jnvZ-k
+
+package pkg01_poojava;
+
+/**
+ *
+ * @author GAMER
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
